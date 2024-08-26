79f9f089366b56cbed899d3e44abe64335f5eb52
Carlos Eduardo Oliveira Alves <carlos.eduardo.oliver.alves@gmail.com>
2024-08-26 11:38:04 -0300

    Close menu function

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ document.querySelector('.menu-hamburguer').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('nav-active');
     document.querySelector('.menu-hamburguer').classList.toggle('toggle');
 });
+
+// Fechar o menu quando clicar fora
+document.addEventListener('click', (event) => {
+    const isClickInsideMenu = document.querySelector('.nav-links').contains(event.target);
+    const isClickOnHamburger = document.querySelector('.menu-hamburguer').contains(event.target);
+    
+    if (!isClickInsideMenu && !isClickOnHamburger) {
+        document.querySelector('.nav-links').classList.remove('nav-active');
+        document.querySelector('.menu-hamburguer').classList.remove('toggle');
+    }
+});
