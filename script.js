65cd7e873931e24115b1c6e2bbf174b4b88a24e1
Carlos Eduardo Oliveira Alves <carlos.eduardo.oliver.alves@gmail.com>
2024-08-26 11:33:22 -0300

    Add Response

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-hamburguer').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('nav-active');
+    document.querySelector('.menu-hamburguer').classList.toggle('toggle');
+});
